214248542410ac89bb9e84832833b1333c2dbfa6
Chea Chanbo <cbo75920@gmail.com>
2024-06-27 23:40:27 +0700

    add time add date

--- app.js
+++ app.js
@@ -20,3 +20,40 @@ function updateTimer() {
 
 // Update timer every second
 setInterval(updateTimer, 1000);
+
+/* real time */
+// function updateTime() {
+//   const now = new Date();
+//   const hours = now.getHours();
+//   const minutes = now.getMinutes();
+//   const seconds = now.getSeconds();
+//   const timeString = `${hours}:${minutes < 10 ? "0" + minutes : minutes}:${
+//     seconds < 10 ? "0" + seconds : seconds
+//   }`;
+//   document.getElementById("time").textContent = timeString;
+// }
+// updateTime();
+// setInterval(updateTime, 1000); // Update every second
+
+/* all date and time */
+function updateDateTime() {
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+  const day = now.getDate();
+  const month = now.getMonth() + 1; // January is 0 in JavaScript
+  const year = now.getFullYear();
+
+  const timeString = `${hours}:${minutes < 10 ? "0" + minutes : minutes}:${
+    seconds < 10 ? "0" + seconds : seconds
+  }`;
+  const dateString = `${day}/${month}/${year}`;
+
+  document.getElementById(
+    "datetime"
+  ).innerHTML = `<p>Time: ${timeString}</p><p>Date: ${dateString}</p>`;
+}
+
+updateDateTime();
+setInterval(updateDateTime, 1000); // Update every second
